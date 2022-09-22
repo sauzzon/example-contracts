03656f9ab34b17ce0467627939cca2a6ff32af15
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-09-22 15:09:36 +0545

    poe3

--- to-be-completed/ProofOfExistence3.sol
+++ to-be-completed/ProofOfExistence3.sol
@@ -1,47 +1,35 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
 
 contract ProofOfExistence3 {
+    mapping(bytes32 => bool) private proofs;
 
-  mapping (bytes32 => bool) private proofs;
-  
-  // store a proof of existence in the contract state
-  function storeProof(bytes32 proof) 
-    internal 
-  {
+    // store a proof of existence in the contract state
+    function storeProof(bytes32 proof) internal {
+        proofs[proof] = true;
+    }
 
-  }
-  
-  // calculate and store the proof for a document
-  function notarize(string memory document) 
-    public 
-  { 
+    // calculate and store the proof for a document
+    function notarize(string memory document) public {
+        storeProof(proofFor(document));
+    }
 
-  }
-  
-  // helper function to get a document's keccak256 hash
-  function proofFor(string memory document) 
-    pure 
-    private 
-    returns (bytes32) 
-  {
- 
-  }
-  
-  // check if a document has been notarized
-  function checkDocument(string memory document) 
-    public 
-    view 
-    returns (bool) 
-  {
+    // helper function to get a document's keccak256 hash
+    function proofFor(string memory document) private pure returns (bytes32) {
+        return keccak256(abi.encode(document));
+    }
 
-  }
+    // check if a document has been notarized
+    function checkDocument(string memory document) public view returns (bool) {
+        return hasProof(proofFor(document));
+    }
 
-  // returns true if proof is stored
-  function hasProof(bytes32 proof) 
-    internal 
-    view 
-    returns(bool) 
-  {
-
-  }
+    // returns true if proof is stored
+    function hasProof(bytes32 proof) internal view returns (bool) {
+        if (proofs[proof] == true) {
+            return true;
+        }
+        return false;
+    }
 }
